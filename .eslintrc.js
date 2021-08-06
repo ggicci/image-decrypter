33510abacecb16c496eaa91fd8f6072dfcc5f840
Ggicci <ggicci.t@gmail.com>
2021-08-06 14:07:41 +0800

    feat: remove prettier

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {},
-  extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-  ],
-}
